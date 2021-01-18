cef0e049861c1f690fc38046b28e49ad874f38c0
adifsgaid <infosgaid@gmail.com>
2021-01-18 11:25:07 +0100

    added slug method and avg score

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,3 +1,12 @@
 class Currency < ApplicationRecord
   has_many :reviews
+  before_create :slugify
+
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    reviews.average(:rating).round(2).to_f
+  end
 end
