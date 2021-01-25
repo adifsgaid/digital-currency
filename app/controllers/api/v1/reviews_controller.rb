067d924601b23b3549a932f60b0493e84c1bcdd6
adifsgaid <infosgaid@gmail.com>
2021-01-25 19:06:25 +0100

    added create and destroy on reviews controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,3 +1,32 @@
-class ReviewsController < ApplicationController
+module Api
+  module V1
+    class ReviewsController < ApplicationController
     
+      def create
+        review = Review.new(review_params)
+
+        if review.save
+          render json: ReviewSerializer.new(review).serialized_json
+        else
+          render json: { error: review.errors.full_messages }, status: 422
+        end
+      end
+
+      def destroy
+        review = review.find_by(:id)
+
+        if review.destroy
+          head :no_content
+        else
+          render json: { error: review.errors.full_messages }, status: 422
+        end
+      end
+
+      private
+
+      def review_params
+        params.require(:reviews).permit(:title, :description, :rating)
+      end
+    end
+  end
 end
