aeec3176e6aa24a4ed4c656fb61fd7fa5e6dab77
adifsgaid <infosgaid@gmail.com>
2021-01-25 17:45:29 +0100

    added seed

--- app/serializers/currency_serializer.rb
+++ app/serializers/currency_serializer.rb
@@ -1,4 +1,6 @@
 class CurrencySerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :img_url, :slug
+
+  has_many :reviews
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Currency.create([
+                  {
+                    name: 'Bitcoin',
+                    image_url: 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/svg/black/bch.svg'
+                  },
+                  {
+                    name: '$PAC',
+                    image_url: 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/svg/black/$pac.svg'
+                  },
+                  {
+                    name: '0XBTC',
+                    image_url: 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/svg/black/0xbtc.svg'
+                  },
+                  {
+                    name: 'AION',
+                    image_url: 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/svg/black/aion.svg'
+                  },
+                  {
+                    name: 'ARN',
+                    image_url: 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/svg/black/arn.svg'
+                  },
+                  {
+                    name: 'CNY',
+                    image_url: 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/svg/black/cny.svg'
+                  }
+                ])
