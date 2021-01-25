5ab88bf2dbbf58d716f12cdeef1e7b8e2b99775a
adifsgaid <infosgaid@gmail.com>
2021-01-25 19:01:01 +0100

    CRUD on currencies controller

--- app/controllers/api/v1/currencies_controller.rb
+++ app/controllers/api/v1/currencies_controller.rb
@@ -4,20 +4,40 @@ module Api
       def index
         currencies = Currency.all
 
-        render json: CurrencySerializer.new(currencies).serialized_json
+        render json: CurrencySerializer.new(currencies, options).serialized_json
       end
 
       def show
         currency = Currency.find(slug: params[:slug])
 
-        render json: CurrencySerializer.new(currencies).serialized_json
+        render json: CurrencySerializer.new(currency, options).serialized_json
       end
 
       def create
         currency = Currency.new(currency_params)
 
         if currency.save
-          render json: CurrencySerializer.new(currencies).serialized_json
+          render json: CurrencySerializer.new(currency).serialized_json
+        else
+          render json: { error: currency.errors.full_messages }, status: 422
+        end
+      end
+
+      def update
+        currency = Currency.find_by(:slug)
+
+        if currency.update(currency_params)
+          render json: CurrencySerializer.new(currency, options).serialized_json
+        else
+          render json: { error: currency.errors.full_messages }, status: 422 # it's unprosseced entity
+        end
+      end
+
+      def destroy
+        currency = Currency.find_by(:slug)
+
+        if currency.destroy
+          head :no_content
         else
           render json: { error: currency.errors.full_messages }, status: 422
         end
@@ -27,6 +47,11 @@ module Api
 
       def currency_params
         params.require(:currency).permit(:name, :img_url)
+      end
+
+      # we created so we can do a coumpund get request to each currency whit their reviews
+      def options
+        @options ||= { include: %i[reviews] }
       end
     end
   end
