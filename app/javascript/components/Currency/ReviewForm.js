903fde07531c94c65700bbfaa330e82d67b6000a
adifsgaid <infosgaid@gmail.com>
2021-03-09 18:51:03 +0100

    styled component in review form

--- app/javascript/components/Currency/ReviewForm.js
+++ app/javascript/components/Currency/ReviewForm.js
@@ -5,6 +5,7 @@ import Selected from "./Stars/Selected";
 import Hover from "./Stars/Hover";
 
 const RatingContainer = styled.div`
+  margin-top: 40px;
   font-family: sans-serif;
   text-align: center;
   border-radius: 4px;
@@ -12,6 +13,8 @@ const RatingContainer = styled.div`
   padding: 40px 0px 10px 0px;
   border: 1px solid #e6e6e6;
   background: #fff;
+  margin-left: 20px;
+  width: 94%;
 `;
 
 const RatingStars = styled.div`
@@ -45,7 +48,75 @@ const RatingStars = styled.div`
     background-image: url("data:image/svg+xml;charset=UTF8,${Hover}");
   }
 `;
-const RatingTitle = styled.div``;
+const RatingTitle = styled.div`
+  font-size: 20px;
+  padding-bottom: 20px;
+  font-weight: bold;
+`;
+
+const Wrapper = styled.div`
+  background: white;
+  padding: 20px;
+  margin-left: 15px;
+  border-radius: 0;
+  padding-bottom: 80px;
+  border-left: 1px solid rgba(0, 0, 0, 0.1);
+  height: 100vh;
+  padding-top: 100px;
+  background: black;
+  padding-right: 80px;
+`;
+
+const Field = styled.div`
+  margin-top: 40px;
+  border-radius: 4px;
+  input {
+    width: 90%;
+    min-height: 50px;
+    border-radius: 4px;
+    border: 1px solid #e6e6e6;
+    margin: 12px 0;
+    padding: 12px;
+    margin-left: 20px;
+  }
+
+  textarea {
+    width: 90%;
+    min-height: 80px;
+    border-radius: 4px;
+    border: 1px solid #e6e6e6;
+    margin: 12px 0;
+    padding: 12px;
+    margin-left: 20px;
+  }
+`;
+
+const SubmitBtn = styled.div`
+  margin-top: 80px;
+  color: #fff;
+  background-color: #71b406;
+  border-radius: 4px;
+  padding: 12px 12px;
+  border: 1px solid #71b406;
+  width: 90%;
+  margin-left: 20px;
+  font-size: 18px;
+  cursor: pointer;
+  transition: ease-in-out 0.2s;
+  &:hover {
+    background: #6dad05;
+    border-color: #5d9405;
+  }
+  text-align: center;
+`;
+
+const HeadLine = styled.div`
+  margin-left: 70px;
+  font-size: 20px;
+  padding: 15px 0;
+  font-weight: bold;
+  color: #fff;
+`;
 
 export const ReviewForm = (props) => {
   const RatingOptions = [5, 4, 3, 2, 1].map((rating, index) => {
@@ -67,11 +138,11 @@ export const ReviewForm = (props) => {
   return (
     <div className="wrapper">
       <form onSubmit={props.handleSubmit}>
-        <div>
+        <HeadLine>
           What do you think about this {props.attributes.name}? share your
           thoghts
-        </div>
-        <div className="field">
+        </HeadLine>
+        <Field>
           <input
             onChange={props.handleChange}
             type="text"
@@ -79,8 +150,8 @@ export const ReviewForm = (props) => {
             placeholder="Review Title "
             value={props.review.title}
           />
-        </div>
-        <div className="field">
+        </Field>
+        <Field>
           <input
             type="text"
             name="description"
@@ -88,14 +159,14 @@ export const ReviewForm = (props) => {
             onChange={props.handleChange}
             value={props.review.description}
           />
-        </div>
-        <div className="field">
+        </Field>
+        <Field>
           <RatingContainer>
-            <div className="rating-title-text"> Rate This Currency</div>
+            <RatingTitle> Rate This Currency</RatingTitle>
             <RatingStars>{RatingOptions}</RatingStars>
           </RatingContainer>
-        </div>
-        <button type="submit"> Submit Your Thoghts</button>
+        </Field>
+        <SubmitBtn type="submit"> Submit Your Thoghts</SubmitBtn>
       </form>
     </div>
   );
