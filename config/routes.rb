16dba6d87c1579a1646cb0417b290962549c6f37
adifsgaid <infosgaid@gmail.com>
2021-05-12 12:38:48 +0200

    Changes in Routes File

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   root 'pages#index'
-
-  namespace :api do
+    namespace :api do
+  
     namespace :v1 do
+  
       resources :currencies, param: :slug
+  
       resources :reviews, only: %i[create destroy]
+  
     end
+  
   end
+  
   get '*path', to: 'pages#index', via: :all
   # This will allow to use react router whitout interfering whit rails route
 end
